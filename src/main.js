112cc788e8e9763fe0810dff0d25779aa83e3301
frontkdi <111939792+frontkdi@users.noreply.github.com>
2023-11-23 15:42:23 +0900

    JS-ArrowUp

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 const header = document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-  console.log(window.scrollY);
   if(window.scrollY > headerHeight){
     header.classList.add('header-dark');
   }
@@ -16,4 +15,15 @@ const home = document.querySelector('.home_container');
 const homeHeight = home.offsetHeight;
 document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
-})
+});
+
+// Arrow up 버튼을 아래로 스크롤 시 투명하게 처리함
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if(window.scrollY > homeHeight / 2){
+    arrowUp.style.opacity = 1;
+  }
+  else {
+    arrowUp.style.opacity = 0;
+  }
+});
